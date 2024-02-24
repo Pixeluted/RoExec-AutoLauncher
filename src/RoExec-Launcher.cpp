b02d56c691049c273866388651ceda80eca4c3c1
Pixeluted <long.iphone@skiff.com>
2024-02-24 08:50:20 +0100

    Removed the need of launch.cfg

--- src/RoExec-Launcher.cpp
+++ src/RoExec-Launcher.cpp
@@ -4,43 +4,38 @@
 namespace fs = std::filesystem;
 
 BOOL validateEnviroment() {
-	if (fs::exists("launch.cfg")) {
-		WIN32_FIND_DATA findFileData;
-		HANDLE hFind = FindFirstFile(L"*.exe", &findFileData);
+	WIN32_FIND_DATA findFileData;
+	HANDLE hFind = FindFirstFile(L"*.exe", &findFileData);
 
-		if (hFind == INVALID_HANDLE_VALUE) {
-			MessageBoxA(NULL, "Failed to find loader!", "Error", MB_OK);
-			return FALSE;
-		}
-
-		wchar_t ownPath[MAX_PATH];
-		if (!GetModuleFileName(NULL, ownPath, MAX_PATH)) {
-			MessageBoxA(NULL, "Failed to get own executable name!", "Error", MB_OK);
-			FindClose(hFind);
-			return FALSE;
-		}
-
-		do {
-			wchar_t fullPath[MAX_PATH];
-			GetCurrentDirectory(MAX_PATH, fullPath);
-			wcscat_s(fullPath, L"\\");
-			wcscat_s(fullPath, findFileData.cFileName);
-
-			if (_wcsicmp(fullPath, ownPath) == 0) {
-				continue;
-			}
-
-			loaderPath = fullPath;
-			std::wcout << L"Found loader: " << fullPath << std::endl;
-
-			break;
-		} while (FindNextFile(hFind, &findFileData) != 0);
-
-		return TRUE;
-	}
-	else {
+	if (hFind == INVALID_HANDLE_VALUE) {
+		MessageBoxA(NULL, "Failed to find loader!", "Error", MB_OK);
 		return FALSE;
 	}
+
+	wchar_t ownPath[MAX_PATH];
+	if (!GetModuleFileName(NULL, ownPath, MAX_PATH)) {
+		MessageBoxA(NULL, "Failed to get own executable name!", "Error", MB_OK);
+		FindClose(hFind);
+		return FALSE;
+	}
+
+	do {
+		wchar_t fullPath[MAX_PATH];
+		GetCurrentDirectory(MAX_PATH, fullPath);
+		wcscat_s(fullPath, L"\\");
+		wcscat_s(fullPath, findFileData.cFileName);
+
+		if (_wcsicmp(fullPath, ownPath) == 0) {
+			continue;
+		}
+
+		loaderPath = fullPath;
+		std::wcout << L"Found loader: " << fullPath << std::endl;
+
+		break;
+	} while (FindNextFile(hFind, &findFileData) != 0);
+
+	return TRUE;
 }
 
 void launchRoExec() {
